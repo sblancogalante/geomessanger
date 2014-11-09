86736876b0d1de8d452e2ec79287f0da2c9ca1dd
sblancogalante@gmail.com <sblancogalante@gmail.com@d983136b-b5ad-19ef-2574-828537f322e8>
2014-11-09 19:24:58 +0000

    Agregue el DML que nos permite tener usuarios por defecto y sectores, paises y documentos por defecto. En la base de datos se agregan bien, falta probar que funcionen las consultas

---
+++ Server/laboratorio_ticDML.sql
@@ -0,0 +1,423 @@
+-- Este es el documento con las actualizaciones a la BD, para generar algunos datos de entrada
+
+-- Empleados
+
+INSERT INTO `laboratorio_tic`.`Employees`
+(
+`document`,
+`iD`,
+`name`,	
+`lastName`, 
+`userName`,
+`password`,
+`location`,
+`sector`,
+`mail`,
+`position`, 
+`status`,
+`admin`)
+VALUES
+(
+"Cedula",
+"5.062.081-0",
+"Santiago",
+"Blanco",
+"sblanco",
+"asdfg12345",
+"Uruguay",
+"Desarrollo",
+"sblanco1@correo.um.edu.uy",
+"Programador",
+0,
+1
+);
+
+INSERT INTO `laboratorio_tic`.`Employees`
+(
+`document`,
+`iD`,
+`name`,	
+`lastName`, 
+`userName`,
+`password`,
+`location`,
+`sector`,
+`mail`,
+`position`, 
+`status`,
+`admin`)
+VALUES
+(
+"Cedula",
+"4.670.218-5",
+"Luis",
+"Gurmendez",
+"lgurmendez",
+"qwerty123456",
+"Uruguay",
+"Desarrollo",
+"lgurmendez@correo.um.edu.uy",
+"Programador",
+0,
+1
+);
+
+-- Tipos generales
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Uruguay"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Chile"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Argentina"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Brasil"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Colombia"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Ecuador"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Venezuela"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Paraguay"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Peru"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Bolivia"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Pais",
+"Estados Unidos"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Comercial"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Ventas"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Recursos Humanos"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Marketing"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Gerencia"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Administracion"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Desarrollo"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Construccion"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Implantacion"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Implementacion"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Testing"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Mantenimiento"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Soporte"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Requerimientos"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Sector",
+"Gestion de calidad"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Documento",
+"Cedula"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Documento",
+"Credencial"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Documento",
+"Pasaporte"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Documento",
+"Libreta de consucir"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Documento",
+"Indocumentado"
+);
+
+INSERT INTO `laboratorio_tic`.`Types`
+(
+`type`,
+`value`
+)
+VALUES
+(
+"Documento",
+"Documento temporal"
+);
+
+
+
+
+
+
